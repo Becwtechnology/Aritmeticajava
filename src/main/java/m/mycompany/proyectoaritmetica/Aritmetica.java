f3494f78bebd9a33be6afe9c1cd6cc8087f33f62
bodega <bodega@BECW-TECHNOLOGY>
2020-02-09 14:25:19 -0600

    ejerccioaritmeticca1

---
+++ src/main/java/m/mycompany/proyectoaritmetica/Aritmetica.java
@@ -0,0 +1,24 @@
+
+package m.mycompany.proyectoaritmetica;
+
+/**
+  * @author Becw Technology
+ */
+public class Aritmetica {
+    //Creacion de un metodo(Realizar la suma de los argumentos y poner el resultado en una variable)
+    //Modificador de acceso(public),tipo de regreso(int),nombre del metodo(sumar)
+    //Argumentos separados por coma (int a,int):
+    
+    public int sumar(int a,int b){
+           int resultado = a+b;
+           return resultado;
+           
+    
+    }
+    
+    
+    
+    
+    
+    
+}
---
+++ src/main/java/m/mycompany/proyectoaritmetica/pruebaritmetica.java
@@ -0,0 +1,25 @@
+
+package m.mycompany.proyectoaritmetica;
+
+/**
+ *
+ * @author Becw Tehnology
+ */
+public class pruebaritmetica {
+    
+    public static void main(String[] args) {
+    
+    //Creacion del objeto tipo aritmetica
+    
+    Aritmetica almacenar = new Aritmetica();
+    
+    int resultado = almacenar.sumar(5, 3);
+    
+    System.out.println("Resultado: " + resultado);
+    
+    
+    
+    }
+   
+        
+}
