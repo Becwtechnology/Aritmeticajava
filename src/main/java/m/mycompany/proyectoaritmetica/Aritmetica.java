3a2fb6f2b7a5980c803cb2930fb1fa58e87f11ac
bodega aurrera <bodega aurrera@DESKTOP-1HTHC08>
2020-02-10 10:07:52 +0100

    version2

--- src/main/java/m/mycompany/proyectoaritmetica/Aritmetica.java
+++ src/main/java/m/mycompany/proyectoaritmetica/Aritmetica.java
@@ -5,19 +5,46 @@ package m.mycompany.proyectoaritmetica;
   * @author Becw Technology
  */
 public class Aritmetica {
-    //Creacion de un metodo(Realizar la suma de los argumentos y poner el resultado en una variable)
+ 
+    //atributos a nivel global de la clase
+    int a;
+    int b;
+    
+
+    
+    //2.-Contructor vacio se debe de llamar exactamente igual que la clase
+    
+    public Aritmetica() {
+       System.out.println("Ejecutando contructor vacio");  
+        }
+    
+    //4.-Constructor con argumentos
+    public Aritmetica(int a, int b) {
+       this.a= a;
+       this.b= b;
+           } 
+    
+    
+    //1.-Creacion de un metodo
+    //(Realizar la suma de los argumentos y poner el resultado en una variable)
     //Modificador de acceso(public),tipo de regreso(int),nombre del metodo(sumar)
     //Argumentos separados por coma (int a,int):
     
-    public int sumar(int a,int b){
-           int resultado = a+b;
-           return resultado;
-           
-    
+    public int sumar(){
+           return this.a + this.b;
     }
     
+    public int restar(){
+           return this.a - this.b;
+    }
     
+    public int multiplicar(){
+            return this.a * this.b;
+    }
     
+    public int dividir(){
+            return this.a / this.b;
+    }
     
     
     
--- src/main/java/m/mycompany/proyectoaritmetica/pruebaritmetica.java
+++ src/main/java/m/mycompany/proyectoaritmetica/pruebaritmetica.java
@@ -2,24 +2,58 @@
 package m.mycompany.proyectoaritmetica;
 
 /**
- *
+ * /**
+    //Creacion del objeto tipo aritmetica
+    
+    Aritmetica almacenar = new Aritmetica();
+    almacenar.a= 10;
+    almacenar.b= 3;
+    int resultado = almacenar.sumar();
+    
+    System.out.println("Resultado: " + resultado);
+          
+    
+    //Creacion de un segundo objeto
+    
+    Aritmetica almacenar2 = new Aritmetica(4,2);
+    System.out.println("Resultado2: " + almacenar2.sumar());
+    
+   
+    }*/
+  
+ /** 
  * @author Becw Tehnology
  */
 public class pruebaritmetica {
     
     public static void main(String[] args) {
-    
-    //Creacion del objeto tipo aritmetica
-    
-    Aritmetica almacenar = new Aritmetica();
-    
-    int resultado = almacenar.sumar(5, 3);
-    
-    System.out.println("Resultado: " + resultado);
-    
-    
-    
-    }
    
+    //variables locales
+    int operandoA = 6;   
+    int operandoB = 2;
+    
+    //crear un objeto de la clase aritmetcia enviando argumentos
+    Aritmetica objeto1 = new Aritmetica(operandoA, operandoB);
+    
+   //Imprimir el valor de los operandos
+   System.out.println("OperandoA =" + operandoA);
+   System.out.println("OperandoB =" + operandoB);     
+        
+   //imprimimos la suma de las variables
+   System.out.println("\nSuma de ambos valores =" + objeto1.sumar()); 
+        
+   //imprimimos la resta de las variables
+   System.out.println("\nResta de ambos valores =" + objeto1.restar());      
+        
+   //imprimimos la multiplicacion de las variables
+   System.out.println("\nMultiplicacion de ambos valores =" + objeto1.multiplicar());      
+        
+   //imprimimos la suma de las variables
+   System.out.println("\nDivision de ambos valores =" + objeto1.dividir());      
+        
+        
+        
+        
         
 }
+    }
